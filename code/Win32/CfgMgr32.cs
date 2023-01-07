954bf092aa046629bd716c4b3d44c1bcd9a58c39
Jason Curl <jcurl@arcor.de>
2023-01-07 22:02:46 +0100

    CfgMgr32: Add API for parsing strings

---
+++ code/Win32/CfgMgr32.cs
@@ -0,0 +1,63 @@
+﻿namespace RJCP.Native.Win32
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using System.Security;
+    using System.Text;
+    using Microsoft.Win32.SafeHandles;
+    using SafeHandles;
+
+    [SuppressUnmanagedCodeSecurity]
+    internal static partial class CfgMgr32
+    {
+        private const int MaxLengthStack = 128;
+
+        [DllImport("cfgmgr32.dll", SetLastError = false, ExactSpelling = true)]
+        public static extern CONFIGRET CM_Get_Child(out SafeDevInst childDevInst, SafeDevInst devInst, int flags);
+
+        [DllImport("cfgmgr32.dll", SetLastError = false, CharSet = CharSet.Unicode, ExactSpelling = true, EntryPoint = "CM_Get_Device_IDW")]
+        public static extern CONFIGRET CM_Get_Device_ID(SafeDevInst devInst, StringBuilder buffer, int bufferLen, int flags);
+
+        [DllImport("cfgmgr32.dll", SetLastError = false, ExactSpelling = true)]
+        public static extern CONFIGRET CM_Get_Device_ID_Size(out int length, SafeDevInst devInst, int flags);
+
+        [DllImport("cfgmgr32.dll", SetLastError = false, CharSet = CharSet.Unicode, ExactSpelling = true, EntryPoint = "CM_Get_Device_ID_ListW")]
+        private static extern CONFIGRET CM_Get_Device_ID_List(string filter, [Out] char[] buffer, int length, int flags);
+
+        [DllImport("cfgmgr32.dll", SetLastError = false, CharSet = CharSet.Unicode, ExactSpelling = true, EntryPoint = "CM_Get_Device_ID_List_SizeW")]
+        private static extern CONFIGRET CM_Get_Device_ID_List_Size(out int length, string filter, int flags);
+
+        [DllImport("cfgmgr32.dll", SetLastError = false, CharSet = CharSet.Unicode, ExactSpelling = true, EntryPoint = "CM_Get_DevNode_Registry_PropertyW")]
+        private static extern CONFIGRET CM_Get_DevNode_Registry_Property(SafeDevInst devInst, CM_DRP property, out int dataType, IntPtr buffer, ref int bufferLen, int flags);
+
+        [DllImport("cfgmgr32.dll", SetLastError = false, CharSet = CharSet.Unicode, ExactSpelling = true, EntryPoint = "CM_Get_DevNode_Registry_PropertyW")]
+        private static extern CONFIGRET CM_Get_DevNode_Registry_Property(SafeDevInst devInst, CM_DRP property, out int dataType, out int buffer, ref int bufferLen, int flags);
+
+        [DllImport("cfgmgr32.dll", SetLastError = false, CharSet = CharSet.Unicode, ExactSpelling = true, EntryPoint = "CM_Get_DevNode_Registry_PropertyW")]
+        private static extern CONFIGRET CM_Get_DevNode_Registry_Property(SafeDevInst devInst, CM_DRP property, out int dataType, [Out] char[] buffer, ref int bufferLen, int flags);
+
+        [DllImport("cfgmgr32.dll", SetLastError = false, CharSet = CharSet.Unicode, ExactSpelling = true, EntryPoint = "CM_Get_DevNode_Registry_PropertyW")]
+        private static unsafe extern CONFIGRET CM_Get_DevNode_Registry_Property(SafeDevInst devInst, CM_DRP property, out int dataType, char* buffer, ref int bufferLen, int flags);
+
+        public static CONFIGRET CM_Get_DevNode_Registry_Property(SafeDevInst devInst, CM_DRP property, out int dataType, out int value)
+        {
+            int length = 4;
+            return CM_Get_DevNode_Registry_Property(devInst, property, out dataType, out value, ref length, 0);
+        }
+
+        [DllImport("cfgmgr32.dll", SetLastError = false, ExactSpelling = true)]
+        public static extern CONFIGRET CM_Get_DevNode_Status(out DN_STATUS status, out int problem, SafeDevInst devInst, int flags);
+
+        [DllImport("cfgmgr32.dll", SetLastError = false, ExactSpelling = true, EntryPoint = "CM_Get_Parent")]
+        public static extern CONFIGRET CM_Get_Parent(out SafeDevInst parentDevInst, SafeDevInst devInst, int flags);
+
+        [DllImport("cfgmgr32.dll", SetLastError = false, ExactSpelling = true)]
+        public static extern CONFIGRET CM_Get_Sibling(out SafeDevInst childDevInst, SafeDevInst devInst, int flags);
+
+        [DllImport("cfgmgr32.dll", SetLastError = false, CharSet = CharSet.Unicode, ExactSpelling = true, EntryPoint = "CM_Locate_DevNodeW")]
+        public static extern CONFIGRET CM_Locate_DevNode(out SafeDevInst devInst, string devInstId, CM_LOCATE_DEVINST flags);
+
+        [DllImport("cfgmgr32.dll", SetLastError = false, ExactSpelling = true, EntryPoint = "CM_Open_DevNode_Key")]
+        public static extern CONFIGRET CM_Open_DevNode_Key(SafeDevInst devInst, Kernel32.REGSAM samDesired, int hardwareProfile, RegDisposition disposition, out SafeRegistryHandle device, int flags);
+    }
+}
---
+++ code/Win32/SafeHandles/SafeDevInst.cs
@@ -0,0 +1,61 @@
+﻿namespace RJCP.Native.Win32.SafeHandles
+{
+    using System;
+    using System.Runtime.ConstrainedExecution;
+    using Microsoft.Win32.SafeHandles;
+
+    /// <summary>
+    /// Gets a handle to the CfgMgr32 DevInst object.
+    /// </summary>
+    public class SafeDevInst : SafeHandleZeroOrMinusOneIsInvalid
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SafeDevInst"/> class.
+        /// </summary>
+        public SafeDevInst() : base(true) { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SafeDevInst"/> class.
+        /// </summary>
+        /// <param name="newHandle">The handle to assign to this object.</param>
+        public SafeDevInst(IntPtr newHandle) : base(true)
+        {
+            handle = newHandle;
+        }
+
+        /// <summary>
+        /// Executes the code required to free the handle.
+        /// </summary>
+        /// <returns>
+        /// Returns <see langword="true"/> if the handle is released successfully; otherwise, in the event of a
+        /// catastrophic failure, <see langword="false"/>. In this case, it generates a releaseHandleFailed MDA Managed
+        /// Debugging Assistant.
+        /// </returns>
+        [ReliabilityContract(Consistency.WillNotCorruptState, Cer.MayFail)]
+        protected override bool ReleaseHandle()
+        {
+            // Here, we must obey all rules for constrained execution regions.
+
+            // If ReleaseHandle failed, it can be reported via the "releaseHandleFailed" managed debugging assistant
+            // (MDA). This MDA is disabled by default, but can be enabled in a debugger or during testing to diagnose
+            // handle corruption problems. We do not throw an exception because most code could not recover from the
+            // problem.
+
+            // There is no documentation that this handle should be closed. When trying to close through single stepping
+            // with a debugger, we get an exception on Windows 10. Further, we see that when querying the root multiple
+            // times, the actual value of the handle is constant, thus suggesting that the implementation doesn't need
+            // to be freed.
+
+            // So in effect, we use this class to wrap the result of the Win32 API in a type safe manner, but don't need
+            // the atomic / clean disposal of the object.
+
+            //return Kernel32.CloseHandle(handle);
+
+            // Also, please note, that it is expected that closing this handle has no effect, as when we return a new
+            // handle via DeviceInstance.Handle, we copy it, so if the user closes that copy (which has no effect), it
+            // won't affect us here.
+            SetHandleAsInvalid();
+            return true;
+        }
+    }
+}
